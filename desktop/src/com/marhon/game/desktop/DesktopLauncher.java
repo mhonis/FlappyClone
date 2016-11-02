85b6ee7a61a1a83ca5842ff260e4f7321cb0a65c
Martin Honis <martin.honis@capgemini.com>
2016-11-02 14:07:05 +0100

    Initial commit

---
+++ desktop/src/com/marhon/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.marhon.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.marhon.game.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}

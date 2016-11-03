1eee730beae0dfc18e842982f15ea2158c3eb384
Martin Honis <martin.honis@capgemini.com>
2016-11-03 09:53:35 +0100

    Fix android launcher

--- core/src/com/marhon/game/states/MenuState.java
+++ core/src/com/marhon/game/states/MenuState.java
@@ -18,6 +18,7 @@ public class MenuState extends State {
         super(gsm);
         background = new Texture("bg.png");
         playBtn = new Texture("playbtn.png");
+        cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
     }
 
     @Override
@@ -35,9 +36,10 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(background, 0, 0, FlappyDemo.WIDTH, FlappyDemo.HEIGHT);
-        sb.draw(playBtn, (FlappyDemo.WIDTH / 2) - (playBtn.getWidth() / 2), (FlappyDemo.HEIGHT / 2) - (playBtn.getHeight() / 2));
+        sb.draw(background, 0, 0);
+        sb.draw(playBtn, cam.position.x - (playBtn.getWidth() / 2), cam.position.y - (playBtn.getHeight() / 2));
         sb.end();
     }
 
